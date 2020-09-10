bdf1a67138980054a35fd266fce05574c3e8f2e1
mike lorengo <mikelor@gmail.com>
2020-09-10 16:19:32 -0700

    Add project files.

---
+++ CruzeBio.Shared/CruzeBioHelper.cs
@@ -0,0 +1,191 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+
+namespace CruzeBio.Shared
+{
+    public static class CruzeBioHelper
+    {
+
+        // Used in the Detect Faces and Verify examples.
+// Recognition model 3 is used for feature extraction, use 1 to simply recognize/detect a face. 
+// However, the API calls to Detection that are used with Verify, Find Similar, or Identify must share the same recognition model.
+const string RECOGNITION_MODEL3 = RecognitionModel.Recognition03;
+const string RECOGNITION_MODEL1 = RecognitionModel.Recognition01;
+
+        static string sourcePersonGroup = null;
+        static IFaceClient faceClient;
+        public static bool IsFaceRegistered { get; set; }
+
+        public static bool IsInitialized { get; set; }
+
+        public static string WorkspaceKey
+        {
+            get;
+            set;
+        }
+        public static Action<string> GreetingsCallback { get => greetingsCallback; set => greetingsCallback = value; }
+
+        private static Action<string> greetingsCallback;
+
+        public static IFaceClient Authenticate(string endpoint, string key)
+        {
+            return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
+        }
+        public static void Init(Action throttled = null)
+        {
+            // Authenticate.
+            faceClient = Authenticate(FACE_ENDPOINT, FACE_SUBSCRIPTION_KEY);
+
+            FaceServiceHelper.ApiKey = "b1843365b41247538cffb304d36609b3";
+            if(throttled!=null)
+            FaceServiceHelper.Throttled += throttled;
+
+            WorkspaceKey = Guid.NewGuid().ToString();
+            ImageAnalyzer.PeopleGroupsUserDataFilter = WorkspaceKey;
+            FaceListManager.FaceListsUserDataFilter = WorkspaceKey;
+
+            IsInitialized = true;
+        }
+
+        public static async Task RegisterFaces()
+        {
+
+
+            try
+            {
+                // Create a person group. 
+                string personGroupId = Guid.NewGuid().ToString();
+                sourcePersonGroup = personGroupId; // This is solely for the snapshot operations example
+                Console.WriteLine($"Create a person group ({personGroupId}).");
+                await faceClient.PersonGroup.CreateAsync(personGroupId, personGroupId, recognitionModel: recognitionModel);
+                // The similar faces will be grouped into a single person group person.
+                foreach (var groupedFace in personDictionary.Keys)
+                {
+                    // Limit TPS
+                    await Task.Delay(250);
+                    Person person = await client.PersonGroupPerson.CreateAsync(personGroupId: personGroupId, name: groupedFace);
+                    Console.WriteLine($"Create a person group person '{groupedFace}'.");
+
+                    // Add face to the person group person.
+                    foreach (var similarImage in personDictionary[groupedFace])
+                    {
+                        Console.WriteLine($"Add face to the person group person({groupedFace}) from image `{similarImage}`");
+                        PersistedFace face = await client.PersonGroupPerson.AddFaceFromUrlAsync(personGroupId, person.PersonId,
+                            $"{url}{similarImage}", similarImage);
+                    }
+                }
+
+
+
+                var persongroupId = Guid.NewGuid().ToString();
+                await FaceServiceHelper.CreatePersonGroupAsync(persongroupId,
+                                                        "Xamarin",
+                                                     WorkspaceKey);
+                await FaceServiceHelper.CreatePersonAsync(persongroupId, "Albert Einstein");
+
+                var personsInGroup = await FaceServiceHelper.GetPersonsAsync(persongroupId);
+
+                await FaceServiceHelper.AddPersonFaceAsync(persongroupId, personsInGroup[0].PersonId,
+                                                           "https://upload.wikimedia.org/wikipedia/commons/d/d3/Albert_Einstein_Head.jpg", null, null);
+
+                await FaceServiceHelper.TrainPersonGroupAsync(persongroupId);
+
+
+                IsFaceRegistered = true;
+
+
+            }
+            catch (FaceAPIException ex)
+
+            {
+                Console.WriteLine(ex.Message);
+                IsFaceRegistered = false;
+
+            }
+
+        }
+
+        public static async Task ProcessCameraCapture(ImageAnalyzer e)
+        {
+
+            DateTime start = DateTime.Now;
+
+            await e.DetectFacesAsync();
+
+            if (e.DetectedFaces.Any())
+            {
+                await e.IdentifyFacesAsync();
+                string greetingsText = GetGreettingFromFaces(e);
+
+                if (e.IdentifiedPersons.Any())
+                {
+
+                    if (greetingsCallback != null)
+                    {
+                        DisplayMessage(greetingsText);
+                    }
+
+                    Console.WriteLine(greetingsText);
+                }
+                else
+                {
+                    DisplayMessage("No Idea, who you're.. Register your face.");
+
+                    Console.WriteLine("No Idea");
+
+                }
+            }
+            else
+            {
+               // DisplayMessage("No face detected.");
+
+                Console.WriteLine("No Face ");
+
+            }
+
+            TimeSpan latency = DateTime.Now - start;
+            var latencyString = string.Format("Face API latency: {0}ms", (int)latency.TotalMilliseconds);
+            Console.WriteLine(latencyString);
+        }
+
+        private static string GetGreettingFromFaces(ImageAnalyzer img)
+        {
+            if (img.IdentifiedPersons.Any())
+            {
+                string names = img.IdentifiedPersons.Count() > 1 ? string.Join(", ", img.IdentifiedPersons.Select(p => p.Person.Name)) : img.IdentifiedPersons.First().Person.Name;
+
+                if (img.DetectedFaces.Count() > img.IdentifiedPersons.Count())
+                {
+                    return string.Format("Welcome back, {0} and company!", names);
+                }
+                else
+                {
+                    return string.Format("Welcome back, {0}!", names);
+                }
+            }
+            else
+            {
+                if (img.DetectedFaces.Count() > 1)
+                {
+                    return "Hi everyone! If I knew any of you by name I would say it...";
+                }
+                else
+                {
+                    return "Hi there! If I knew you by name I would say it...";
+                }
+            }
+        }
+
+        static void DisplayMessage(string greetingsText)
+        {
+            greetingsCallback?.Invoke(greetingsText);
+        }
+    }
+}
---
+++ CruzeBio/MainActivity.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.Gms.Vision;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+using CruzeBio.Shared;
+
+namespace CruzeBio
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private static readonly string TAG = "FaceTracker";
+        private static readonly int RC_HANDLE_GMS = 9001;
+        
+        // permission request codes need to be < 256
+        private static readonly int RC_HANDLE_CAMERA_PERM = 2;
+
+        private CameraSource mCameraSource = null;
+        private CameraSourcePreview mPreview;
+        private GraphicOverlay mGraphicOverlay;
+
+        public static string GreetingsText
+        {
+            get;
+            set;
+        }
+
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            mPreview = FindViewById<CameraSourcePreview>(Resource.Id.preview);
+            mGraphicOverlay = FindViewById<GraphicOverlay>(Resource.Id.faceOverlay);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.Camera) == Permission.Granted)
+            {
+                //CreateCameraSource();
+                CruzeBioHelper.Init();
+                CruzeBioHelper.GreetingsCallback = (s) => { RunOnUiThread(()=> GreetingsText = s ); };
+                await CruzeBioHelper.RegisterFaces();
+            }
+            else { RequestCameraPermission(); }
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartCameraSource();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            mPreview.Stop();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (mCameraSource != null)
+            {
+                mCameraSource.Release();
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void RequestCameraPermission()
+        {
+            Log.Warn(TAG, "Camera permission is not granted. Requesting permission");
+
+            var permissions = new string[] { Manifest.Permission.Camera };
+
+            if (!ActivityCompat.ShouldShowRequestPermissionRationale(this,
+                    Manifest.Permission.Camera))
+            {
+                ActivityCompat.RequestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+                return;
+            }
+
+            Snackbar.Make(mGraphicOverlay, Resource.String.permission_camera_rationale,
+                    Snackbar.LengthIndefinite)
+                    .SetAction(Resource.String.ok, (o) => { ActivityCompat.RequestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM); })
+                    .Show();
+        }
+
+
+    }
+}
