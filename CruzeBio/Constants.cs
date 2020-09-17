74aab5d91e3b3541b17f1015b22a1a9150253769
mike lorengo <mikelor@gmail.com>
2020-09-17 16:05:03 -0700

    Quick change

--- CruzeBio/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CruzeBio
-{
-    public static class Constants
-    {
-        // URL of REST service
-        public static string RestUrl = "https://cruze-test-api-westus2.azurewebsites.net{0}";
-    }
-}
